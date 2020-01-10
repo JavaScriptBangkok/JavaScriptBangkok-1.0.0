cd8678cd3e90108ce975365390449e18b203fb90
Thai Pangsakulyanont <dtinth@spacet.me>
2020-01-11 00:57:38 +0700

    Initial work on 2nd phase
    Co-Authored-By: Runyasak Chaengnaimuang <world.jadi@gmail.com>
    Co-Authored-By: Thanyaboon Tovorapan <chaoexit@users.noreply.github.com>

---
+++ website2/components/SpeakerList.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(speaker, index) of speakers" :key="index">
+        <div class="image-wrapper">
+          <img src="https://picsum.photos/120/120">
+        </div>
+        <div class="talk-info">
+          <h3>{{ speaker.name }}</h3>
+          <p>{{ speaker.title }}</p>
+        </div>
+        <!--p>
+          {{ speaker.description }}
+        </p-->
+      </li>  
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+  .image-wrapper > img {
+    border-radius: 50%;
+    height: 120px;
+    width: 120px;
+  }
+  ul {
+    list-style: none;
+    padding: 0;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 16px;
+  }
+  li {
+    background: linear-gradient(90.38deg, #FAE9AD 0.14%, #FFFFFF 50.01%);
+    border-radius: 10px;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+    display: flex;
+    align-items: center;
+    padding: 15px;
+  }
+  .talk-info {
+    text-align: right;
+    margin-left: auto;
+  }
+  h3 {
+    color: #EABA06;
+  }
+  p {
+    color: #767676;
+  }
+</style>
+
+<script>
+export default {
+  name: 'SpeakerList',
+  props: {
+    speakers: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
