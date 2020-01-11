fc1c5bbe8a8af8c463d057fd7dfa643661fbd4f5
Thai Pangsakulyanont <dtinth@spacet.me>
2020-01-11 07:56:51 +0700

    Prettier SpeakerList.vue

--- website2/components/SpeakerList.vue
+++ website2/components/SpeakerList.vue
@@ -1,11 +1,9 @@
 <template>
   <div>
     <ul>
-      <li
-        v-for="(speaker, index) of speakers"
-        :key="index">
+      <li v-for="(speaker, index) of speakers" :key="index">
         <div class="image-wrapper">
-          <img src="https://picsum.photos/120/120">
+          <img src="https://picsum.photos/120/120" />
         </div>
         <div class="talk-info">
           <h3>{{ speaker.name }}</h3>
@@ -14,53 +12,54 @@
         <img
           src="./subtract-frame.svg"
           alt="Subtract Frame"
-          class="subtract-frame">
+          class="subtract-frame"
+        />
         <!--p>
           {{ speaker.description }}
         </p-->
-      </li>  
+      </li>
     </ul>
   </div>
 </template>
 
 <style scoped>
-  .image-wrapper > img {
-    border-radius: 50%;
-    height: 120px;
-    width: 120px;
-  }
-  ul {
-    list-style: none;
-    padding: 0;
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    grid-gap: 16px;
-  }
-  li {
-    background: linear-gradient(90.38deg, #FAE9AD 0.14%, #FFFFFF 50.01%);
-    border-radius: 10px;
-    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
-    display: flex;
-    align-items: center;
-    padding: 30px;
-    position: relative;
-  }
-  .subtract-frame {
-    position: absolute;
-    bottom: 0;
-    left: 50%;
-    transform: translateX(-50%);
-  }
-  .talk-info {
-    text-align: right;
-    margin-left: auto;
-  }
-  h3 {
-    color: #EABA06;
-  }
-  p {
-    color: #767676;
-  }
+.image-wrapper > img {
+  border-radius: 50%;
+  height: 120px;
+  width: 120px;
+}
+ul {
+  list-style: none;
+  padding: 0;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 16px;
+}
+li {
+  background: linear-gradient(90.38deg, #fae9ad 0.14%, #ffffff 50.01%);
+  border-radius: 10px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+  display: flex;
+  align-items: center;
+  padding: 30px;
+  position: relative;
+}
+.subtract-frame {
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  transform: translateX(-50%);
+}
+.talk-info {
+  text-align: right;
+  margin-left: auto;
+}
+h3 {
+  color: #eaba06;
+}
+p {
+  color: #767676;
+}
 </style>
 
 <script>
@@ -69,8 +68,8 @@ export default {
   props: {
     speakers: {
       type: Array,
-      default: () => []
-    }
-  }
+      default: () => [],
+    },
+  },
 }
 </script>
