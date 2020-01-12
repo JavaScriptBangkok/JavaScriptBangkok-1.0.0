ede3bc9a49caef3de1abf3f80a6e7ca5125eef6f
Thai Pangsakulyanont <dtinth@spacet.me>
2020-01-12 22:29:58 +0700

    Add og:image

--- website2/.vuepress/config.js
+++ website2/.vuepress/config.js
@@ -4,7 +4,29 @@ module.exports = {
       'link',
       {
         rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css?family=K2D:400,500,700,800&display=swap',
+        href:
+          'https://fonts.googleapis.com/css?family=K2D:400,500,700,800&display=swap',
+      },
+    ],
+    [
+      'meta',
+      {
+        property: 'og:image',
+        content: '/og.png',
+      },
+    ],
+    [
+      'meta',
+      {
+        property: 'og:title',
+        content: 'JavaScript Bangkok 1.0.0',
+      },
+    ],
+    [
+      'meta',
+      {
+        property: 'og:description',
+        content: '08 Feb 2020 at 09:00–22:00 @ KBank Siam Pic-Ganesha Theatre',
       },
     ],
   ],
