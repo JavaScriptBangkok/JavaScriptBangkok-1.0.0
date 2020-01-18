01bbf5222da1f183cf982c5b118c5942430b5ad4
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2020-01-18 23:18:49 +0700

    Remove comments and unused style

--- website2/.vuepress/local-components/SpeakerModal.vue
+++ website2/.vuepress/local-components/SpeakerModal.vue
@@ -65,15 +65,13 @@ export default {
 
 <style scoped>
 .speaker-modal {
-  /* display: none; */
-  position: fixed; /* Stay in place */
-  z-index: 1; /* Sit on top */
+  position: fixed;
+  z-index: 1;
   left: 0;
   top: 0;
-  width: 100%; /* Full width */
-  height: 100%; /* Full height */
-  overflow: auto; /* Enable scroll if needed */
-  background-color: rgb(0, 0, 0);
+  width: 100%;
+  height: 100%;
+  overflow: auto;
   background-color: rgba(0, 0, 0, 0.4);
 }
 
@@ -85,7 +83,6 @@ export default {
   width: 80%;
   max-width: 960px;
 
-  /* background: linear-gradient(90.38deg, #fae9ad 0.14%, #ffffff 50.01%); */
   border-radius: 10px;
   box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
 
@@ -151,7 +148,6 @@ p {
   color: #767676;
 }
 
-/* The Close Button */
 .close {
   color: #aaa;
   font-size: 28px;
