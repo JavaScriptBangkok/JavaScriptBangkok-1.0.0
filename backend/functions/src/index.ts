0e6892f39ad37ec06ae7bfa3eea22d9d1e09d5fc
siriwatknp <siriwatkunaporn@gmail.com>
2020-01-26 17:00:44 +0700

    init firebase cloud functions

---
+++ backend/functions/src/index.ts
@@ -0,0 +1,13 @@
+import * as functions from 'firebase-functions';
+import admin from 'firebase-admin';
+import serviceAccount from '../service-account.json';
+
+admin.initializeApp({
+ // @ts-ignore
+ credential: admin.credential.cert(serviceAccount),
+ databaseURL: "https://javascriptbangkok-companion.firebaseio.com"
+});
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello from Firebase!");
+});
