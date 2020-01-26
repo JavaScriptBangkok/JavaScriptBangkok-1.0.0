ddaa79f71e4c7acf0cb91cc473b2a778146198d1
Thai Pangsakulyanont <dtinth@spacet.me>
2020-01-27 00:17:02 +0700

    Update food.spec.js

--- e2e/cypress/integration/app/food.spec.js
+++ e2e/cypress/integration/app/food.spec.js
@@ -1,24 +1,26 @@
 describe('Food page', () => {
-  it('displays lunchtime choices');
-  it('displays available slots for each choice');
-  it('displays a countdown timer until time is up');
-  it('becomes unavailable when time is up');
-  it('allows choosing 2 food stalls');
-  it('allows choosing 1 restaurant');
-  it('asks user to sign in when selecting a choice, if unauthenticated');
-  it('lets user customize a menu when selecting a choice, if authenticated');
-});
+  it('displays lunchtime choices')
+  it('displays available slots for each choice')
+  it('displays a countdown timer until time is up')
+  it('becomes unavailable when time is up')
+  it('allows choosing 2 food stalls')
+  it('allows choosing 1 restaurant')
+  it('asks user to sign in when selecting a choice, if unauthenticated')
+  it('lets user customize a menu when selecting a choice, if authenticated')
+})
 describe('Customization section', () => {
-  it('lets user select a customization option');
-  it('allows confirming if not fully booked');
-  it('prevents confirming if fully booked');
-  it('allows changing choice');
-});
+  it('lets user select a customization option')
+  it('shows availability if menu availability is limited')
+  it('prevents selecting menu if menu is fully booked')
+  it('allows confirming if not fully booked')
+  it('prevents confirming if fully booked')
+  it('allows changing choice')
+})
 describe('Customization section', () => {
-  it('displays the selected restaurant');
-  it('displays directions to the restaurant');
-  it('displays the selected customizations');
-  it('lets user view the menu should they need to make changes');
-  it('lets user re-customize their selection within timeframe');
-  it('lets user delete their selection within timeframe');
-});
+  it('displays the selected restaurant')
+  it('displays directions to the restaurant')
+  it('displays the selected customizations')
+  it('lets user view the menu should they need to make changes')
+  it('lets user re-customize their selection within timeframe')
+  it('lets user delete their selection within timeframe')
+})
