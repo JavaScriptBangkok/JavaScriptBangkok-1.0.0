5fc7845b530de4fb258eb23d6a3322306bd87ac2
Thai Pangsakulyanont <dtinth@spacet.me>
2020-01-28 21:51:49 +0700

    Fix cypress

--- e2e/cypress/integration/app/conference.spec.js
+++ e2e/cypress/integration/app/conference.spec.js
@@ -1,27 +1,26 @@
 describe('Conference page', () => {
   beforeEach(() => {
-    cy.usePrototype();
-    cy.viewport('iphone-6');
-  });
+    cy.viewport('iphone-6')
+  })
   it('displays the latest announcement', () => {
-    cy.resetAnnouncement();
-    cy.enterConferenceSection();
-    cy.queryByText(/This is an announcement!/).should('exist');
-  });
+    cy.resetAnnouncement()
+    cy.enterConferenceSection()
+    cy.queryByText(/This is an announcement!/).should('exist')
+  })
   it('can display links in announcement', () => {
-    cy.resetAnnouncement();
-    cy.enterConferenceSection();
-    cy.queryByText('feedback').should('match', 'a[href]');
-  });
-  it('has a tweet button');
+    cy.resetAnnouncement()
+    cy.enterConferenceSection()
+    cy.queryByText('feedback').should('match', 'a[href]')
+  })
+  it('has a tweet button')
   it('displays schedule', () => {
-    cy.enterConferenceSection();
-    cy.queryByText('Schedule').should('exist');
-  });
+    cy.enterConferenceSection()
+    cy.queryByText('Schedule').should('exist')
+  })
   it('updates the announcement in real-time', () => {
-    cy.resetAnnouncement();
-    cy.enterConferenceSection();
-    cy.updateAnnouncement('Networking party will start at 19:00');
-    cy.queryByText('Networking party will start at 19:00').should('exist');
-  });
-});
+    cy.resetAnnouncement()
+    cy.enterConferenceSection()
+    cy.updateAnnouncement('Networking party will start at 19:00')
+    cy.queryByText('Networking party will start at 19:00').should('exist')
+  })
+})
--- e2e/cypress/support/commands.js
+++ e2e/cypress/support/commands.js
@@ -1,45 +1,19 @@
 // @ts-check
-import '@testing-library/cypress/add-commands';
+import '@testing-library/cypress/add-commands'
 
 /* eslint no-loop-func: off */
 
 // https://on.cypress.io/custom-commands
-const testSettings = { usingPrototype: false };
-beforeEach(() => {
-  testSettings.usingPrototype = false;
-});
-
-/** @type {OurCustomCommands} */
-const prototypeCustomCommands = {
-  enterConferenceSection() {
-    cy.visit('/prototype/conference.html');
-  },
-  resetAnnouncement() {},
-  updateAnnouncement(text) {
-    cy.window().then(window => {
-      const element = window.document.getElementById('announcement');
-      element.textContent = text;
-    });
-  }
-};
 
 /** @type {OurCustomCommands} */
 const actualCustomCommands = {
   enterConferenceSection() {
-    cy.visit('/');
+    cy.visit('/')
   },
   resetAnnouncement() {},
-  updateAnnouncement() {}
-};
-
-for (const key of Object.keys(prototypeCustomCommands)) {
-  Cypress.Commands.add(key, (...args) =>
-    (testSettings.usingPrototype
-      ? prototypeCustomCommands
-      : actualCustomCommands)[key](...args)
-  );
+  updateAnnouncement() {},
 }
 
-Cypress.Commands.add('usePrototype', () => {
-  testSettings.usingPrototype = true;
-});
+for (const key of Object.keys(actualCustomCommands)) {
+  Cypress.Commands.add(key, (...args) => actualCustomCommands[key](...args))
+}
