3ba79f11ad6497a722c6a93d5196fa71788f32d2
Thai Pangsakulyanont <dtinth@spacet.me>
2020-01-28 23:29:08 +0700

    Also write announcement to Firestore

--- backend/functions/src/Announcement.ts
+++ backend/functions/src/Announcement.ts
@@ -1,6 +1,10 @@
-import { getEnvRef } from './FirebaseSetup'
+import { getEnvRef, getEnvDoc } from './FirebaseSetup'
 
 export async function updateAnnouncementText(env: string, text: string) {
+  await getEnvDoc(env)
+    .collection('configuration')
+    .doc('announcement')
+    .set({ text }, { merge: true })
   await getEnvRef(env)
     .child('announcement')
     .child('text')
