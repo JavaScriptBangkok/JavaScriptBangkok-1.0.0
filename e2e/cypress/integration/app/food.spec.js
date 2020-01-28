0eba16de4fd085737dfac6d547685195aa2296cf
Thai Pangsakulyanont <dtinth@spacet.me>
2020-01-28 23:02:10 +0700

    Update food specs a bit

--- e2e/cypress/integration/app/food.spec.js
+++ e2e/cypress/integration/app/food.spec.js
@@ -2,11 +2,13 @@ describe('Food page', () => {
   it('displays lunchtime choices')
   it('displays available slots for each choice')
   it('displays a countdown timer until time is up')
-  it('becomes unavailable when time is up')
+  it('becomes unavailable after time is up')
   it('allows choosing 2 food stalls')
   it('allows choosing 1 restaurant')
   it('asks user to sign in when selecting a choice, if unauthenticated')
   it('lets user customize a menu when selecting a choice, if authenticated')
+  it('reduces the menu availability after making a selection')
+  it('reduces the restaurant capacity after making a selection')
 })
 describe('Customization section', () => {
   it('lets user select a customization option')
@@ -16,11 +18,11 @@ describe('Customization section', () => {
   it('prevents confirming if fully booked')
   it('allows changing choice')
 })
-describe('Customization section', () => {
+describe('Order summary section', () => {
   it('displays the selected restaurant')
   it('displays directions to the restaurant')
   it('displays the selected customizations')
   it('lets user view the menu should they need to make changes')
   it('lets user re-customize their selection within timeframe')
-  it('lets user delete their selection within timeframe')
+  it('prevents user from changing their selection after timeframe is passed')
 })
