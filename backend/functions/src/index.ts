9abd711a6dc3f36f211f61b81e2a75be333db52f
Thai Pangsakulyanont <dtinth@spacet.me>
2020-01-28 22:58:11 +0700

    Expose an endpoint to set the announcement for test environment

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,8 +1,23 @@
 import * as functions from 'firebase-functions'
 import { initializeFirebase } from './FirebaseSetup'
+import * as Announcement from './Announcement'
 
 initializeFirebase()
 
-export const helloWorld = functions.region('asia-northeast1').https.onRequest((request, response) => {
-  response.send('Hello from Firebase!')
-})
+export const helloWorld = functions
+  .region('asia-northeast1')
+  .https.onRequest((request, response) => {
+    response.send('Hello from Firebase!')
+  })
+
+export const setTestAnnouncement = functions
+  .region('asia-northeast1')
+  .https.onRequest(async (request, response) => {
+    try {
+      await Announcement.updateAnnouncementText('test', request.body.text)
+      response.json({ ok: true })
+    } catch (e) {
+      response.status(500).json({ ok: false })
+      console.error(e)
+    }
+  })
