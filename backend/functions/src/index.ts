2af00d77209218e37099296ae7cd399aaab33d56
Thai Pangsakulyanont <dtinth@spacet.me>
2020-01-30 02:39:23 +0700

    Add a test endpoint to mint test auth tokens

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,5 +1,6 @@
 import * as functions from 'firebase-functions'
 import { initializeFirebase } from './FirebaseSetup'
+import * as Authentication from './Authentication'
 import * as Announcement from './Announcement'
 
 initializeFirebase()
@@ -16,6 +17,33 @@ export const setTestAnnouncement = functions
     try {
       await Announcement.updateAnnouncementText('test', request.body.text)
       response.json({ ok: true })
+    } catch (e) {
+      response.status(500).json({ ok: false })
+      console.error(e)
+    }
+  })
+
+export const getTestToken = functions
+  .region('asia-northeast1')
+  .https.onRequest(async (request, response) => {
+    try {
+      const uid = String(request.query.uid)
+      const allowedUids = ['test01', 'test02', 'test03', 'test04', 'test05']
+      if (!allowedUids.includes(uid)) {
+        response
+          .status(400)
+          .json({ ok: false, message: 'This uid is not allowed' })
+        return
+      }
+      await Authentication.intializeProfile('test', uid, {
+        firstname: uid,
+        lastname: 'user',
+        email: `${uid}@example.com`,
+        referenceCode: uid.toUpperCase(),
+        ticketType: 'Test Ticket',
+      })
+      const token = await Authentication.mintUserToken(uid)
+      response.json({ ok: true, token: token })
     } catch (e) {
       response.status(500).json({ ok: false })
       console.error(e)
