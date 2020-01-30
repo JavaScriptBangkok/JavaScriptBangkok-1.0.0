b4ba22e508bb1b02182bdd6cf04ee87963f56d67
Thai Pangsakulyanont <dtinth@spacet.me>
2020-01-31 02:22:33 +0700

    Integrate with Eventpop

--- backend/functions/src/Authentication.ts
+++ backend/functions/src/Authentication.ts
@@ -1,6 +1,9 @@
 import admin from 'firebase-admin'
 import { getEnvDoc } from './FirebaseSetup'
 import * as functions from 'firebase-functions'
+import axios from 'axios'
+import querystring from 'querystring'
+import { getConfig } from './Configuration'
 
 export async function mintUserToken(uid: string) {
   return admin.auth().createCustomToken(uid)
@@ -65,4 +68,73 @@ export async function getTestToken(uid: string) {
   })
   const token = await mintUserToken(uid)
   return token
+}
+
+export async function getAccessTokenFromEventpop(
+  code: string,
+): Promise<string> {
+  const tokenResponse = await axios
+    .post(
+      'https://www.eventpop.me/oauth/token',
+      querystring.stringify({
+        client_id: getConfig('EVENTPOP_CLIENT_ID', ['eventpop', 'clientid']),
+        client_secret: getConfig('EVENTPOP_CLIENT_SECRET', [
+          'eventpop',
+          'clientsecret',
+        ]),
+        code: code,
+        redirect_uri:
+          'https://javascriptbangkok.com/1.0.0/eventpop_oauth_callback.html',
+        grant_type: 'authorization_code',
+      }),
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      },
+    )
+    .catch(handleNetworkError('Getting access token'))
+  const accessToken = tokenResponse.data.access_token
+  if (typeof accessToken !== 'string') {
+    throw new Error('Did not get an access token')
+  }
+  return accessToken
+}
+
+export async function getProfilesFromEventpop(
+  accessToken: string,
+): Promise<ProfileData[]> {
+  const ticketResponse = await axios
+    .get('https://www.eventpop.me/api/public/organizers/2444/tickets', {
+      headers: {
+        Authorization: 'Bearer ' + accessToken,
+      },
+      params: {
+        q: JSON.stringify({ event_id: 7210 }),
+      },
+    })
+    .catch(handleNetworkError('Getting access token'))
+  const tickets = ticketResponse.data.tickets
+  if (!Array.isArray(tickets)) {
+    throw new Error('Resulting tickets is not an array, got ' + typeof tickets)
+  }
+  return tickets.map(
+    (ticket): ProfileData => ({
+      firstname: ticket.firstname,
+      lastname: ticket.lastname,
+      email: ticket.email,
+      referenceCode: ticket.reference_code,
+      ticketType: ticket.ticket_type?.name,
+    }),
+  )
+}
+
+function handleNetworkError(action: string) {
+  return (error: Error) => {
+    const data = (error as any).response?.data
+    if (data) {
+      console.error('Error response:', data)
+    }
+    throw new Error('Failed to ' + action + ': ' + error)
+  }
 }
--- backend/functions/src/cli.ts
+++ backend/functions/src/cli.ts
@@ -4,8 +4,10 @@ import * as fs from 'fs'
 
 import { initializeFirebase } from './FirebaseSetup'
 import * as FoodReservation from './FoodReservation'
+import * as Authentication from './Authentication'
 
 initializeFirebase()
+require('dotenv').config()
 
 tkt
   .cli({
@@ -33,4 +35,32 @@ tkt
     await importFood('data/food.yml', 'production')
     log.info('All done!')
   })
+  .command(
+    'check-eventpop-code <code>',
+    'Check Eventpop OAuth2 authorization code and return the Eventpop access token',
+    {
+      code: {
+        desc: 'OAuth2 authorization code received from Eventpop',
+      },
+    },
+    async (args: any) => {
+      const log = tkt.logger('check-eventpop-code')
+      const token = await Authentication.getAccessTokenFromEventpop(args.code)
+      log.info(token)
+    },
+  )
+  .command(
+    'get-eventpop-profile <token>',
+    'Get the profile from Eventpop access token',
+    {
+      token: {
+        desc: 'OAuth2 access token',
+      },
+    },
+    async (args: any) => {
+      const log = tkt.logger('get-eventpop-profile')
+      const profile = await Authentication.getProfilesFromEventpop(args.token)
+      log.info(profile)
+    },
+  )
   .parse()
