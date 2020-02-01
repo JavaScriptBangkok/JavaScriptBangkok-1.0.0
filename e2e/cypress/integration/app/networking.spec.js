6d168d717ea05f5bb987241dd4456508e452a511
Thai Pangsakulyanont <dtinth@spacet.me>
2020-02-02 01:32:03 +0700

    Format code for networking spec

--- e2e/cypress/integration/app/networking.spec.js
+++ e2e/cypress/integration/app/networking.spec.js
@@ -1,12 +1,12 @@
 describe('Networking page', () => {
-  it('asks user to log in if unauthenticated');
-  it("displays the user's badge");
-  it("displays the user's QR code");
-  it("allows users to scan another person's QR code");
-  it('displays a list of obtained badges');
-  it("adds the scanned user's badge to the badge catalog");
-  it("adds the scanned user's profile to the network");
-  it("displays the user's network");
-  it('lets user customize their bio');
-  it("lets user view their network's bio");
-});
+  it('asks user to log in if unauthenticated')
+  it("displays the user's badge")
+  it("displays the user's QR code")
+  it("allows users to scan another person's QR code")
+  it('displays a list of obtained badges')
+  it("adds the scanned user's badge to the badge catalog")
+  it("adds the scanned user's profile to the network")
+  it("displays the user's network")
+  it('lets user customize their bio')
+  it("lets user view their network's bio")
+})
